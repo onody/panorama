fc3ce525462e14ef782693026e2a427a37d12491
Kohei Onodera <onodera212@gmail.com>
2016-04-19 12:54:58 +0900

    fix typo

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,13 +4,13 @@
 # http://en.wikipedia.org/wiki/Cron
 set :output, 'log/cron.log'
 
-every '*/7 * * * *'
+every '*/7 * * * *' do
   runner "Tasks::Bitflyer::TradeTask.new.execute"
 end
-every '*/9 * * * *'
+every '*/9 * * * *' do
   runner "Tasks::Zaif::TradeTask.new.execute"
 end
-every '*/11 * * * *'
+every '*/11 * * * *' do
   runner "Tasks::Coincheck::TradeTask.new.execute"
 end
 # Learn more: http://github.com/javan/whenever
