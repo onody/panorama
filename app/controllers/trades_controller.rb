d8306f79f865a972f2d76f23cd869c4d44a431f2
Kohei Onodera <onodera212@gmail.com>
2016-05-21 22:22:54 +0900

    refactor chart methods

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,37 +1,13 @@
 class TradesController < ApplicationController
   require 'crudable'
   include CRUDable
+  include Chart
 
   def chart
-    @trade_hourly_stats = for_chart_format
+    @trade_hourly_stats = all_chart
 
     respond_to do |format|
       format.json { render json: @trade_hourly_stats }
     end
-  end
-
-  private
-
-  def for_chart_format
-    result = []
-
-    days = ['x']
-    24.downto(1) do |i|
-      days << i.hour.ago.strftime('%m-%d %H')
-    end
-    result << days
-
-    vendors = Vendor.all
-    vendors.each do |v|
-      row = [v.name]
-      24.downto(1) do |i|
-        t = i.hour.ago
-        ths = TradeHourlyStat.where(vendor_id: v.id, hour: Time.new(t.year, t.month, t.day, t.hour)).first
-        row << (ths ? ths.avg_price : nil)
-      end
-      result << row
-    end
-
-    result
   end
 end
